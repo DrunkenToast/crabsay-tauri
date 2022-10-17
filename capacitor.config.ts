8c1cd9f71322507887e81bf94859a97585ab6dea
Peter Leconte <peter5leconte@gmail.com>
2022-10-17 14:54:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-cowsay4k',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
